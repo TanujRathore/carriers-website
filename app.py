8271a3670280e9b3d0d73911aa1cbfcf2f604af1
TanujRathore <91781064+TanujRathore@users.noreply.github.com>
2023-07-22 10:01:45 +0000

    add email functionality

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, jsonify
-
-from database import load_jobs_from_db,load_job_from_db
+from flask import Flask, request,render_template, jsonify
+from flask_mail import Mail, Message
+import os
+from database import load_jobs_from_db,load_job_from_db, add_application_to_db
 
 app=Flask(__name__)
 
+app.config["MAIL_DEFAULT_SENDER"]=os.environ["MAIL_DEFAULT_SENDER"]
+app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
+app.config["MAIL_PORT"]=587
+app.config["MAIL_SERVER"]="smtp.gmail.com"
+app.config["MAIL_USE_TLS"]=True
+app.config["MAIL_USERNAME"]=os.environ["MAIL_USERNAME"]
+mail=Mail(app)
 
 @app.route("/")
 def hello_world():
@@ -22,6 +30,23 @@ def show_job(id):
   if not job:
     return "Not Found",404
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data=request.form
+  job=load_job_from_db(id)
+
+  add_application_to_db(id,data)
+  msg = Message(
+                'Hello',
+                #sender ='yourId@gmail.com',
+                recipients = [data['email']]
+               )
+  msg.body = 'Hello Flask message sent from Flask-Mail'
+  mail.send(msg)
+  
+  return render_template('application_submitted.html',application=data,job=job)
   
 
 if __name__=='__main__':
--- database.py
+++ database.py
@@ -41,6 +41,40 @@ def load_job_from_db(id):
        text("SELECT * FROM jobs WHERE id=:val"),{'val':id})
     row=result.fetchone()
     return row._asdict()
+
+
+'''def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query=text("insert into applications (job_id,full_name,email,linkedin_url,education,work_experience,resume_url) values(:full_name,:email,:linkedin_url,:education,:work_experience,:resume_url)")
+    
+    conn.execute(query,
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin_url=data['linkedin_url'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url'])'''
+
+
+def add_application_to_db(id , data):
+    with engine.connect() as conn:
+
+        stmt = text("INSERT INTO `applications` ( `job_id`, `full_name`, `email`, `linkedin_url`, `education`, `work_experience`, `resume_url`) VALUES ( :job_id, :full_name, :email, :linkedin_url, :education ,  :work_experience,:resume_url)")
+       
+
+
+        stmt = stmt.bindparams(job_id=id)
+        stmt = stmt.bindparams( full_name = data['full_name'])
+        stmt = stmt.bindparams( email = data['email'])
+        stmt = stmt.bindparams( linkedin_url = data['linkedin_url'])
+        stmt = stmt.bindparams( education = data['education'])
+        stmt = stmt.bindparams( work_experience = data['work_experience'])
+        stmt = stmt.bindparams( resume_url = data['resume_url'])
+        conn.execute(stmt)
+
+                 
+
+
     
     
 
